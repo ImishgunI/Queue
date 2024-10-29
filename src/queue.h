95cee496a73b670d1e844909500ee67260dbad47
Michael <mvasilyev04@gmail.com>
2024-10-29 16:15:48 +0300

    add struct List

--- src/queue.h
+++ src/queue.h
@@ -2,12 +2,16 @@
 #define QUEUE_H
 
 /*
-    We need to use 2 pointers, 1 to first element, 2 to last element
+    We need to use 2 pointers, 1 to first element, 2 to last element, 
 */
 
+typedef struct List {
+    int value;
+    struct list* next;
+} List;
 
 typedef struct queue {
-
+    struct List* frnt, *rear;
 } queue;
 
 #endif
