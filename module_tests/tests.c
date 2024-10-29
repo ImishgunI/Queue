d6e4dfe540ac8377b2fe60bafc5d18f26f46b58f
Michael <mvasilyev04@gmail.com>
2024-10-29 21:48:58 +0300

    update test push

--- module_tests/tests.c
+++ module_tests/tests.c
@@ -2,10 +2,13 @@
 
 #include "../src/queue.h"
 
+extern struct queue* head;
+extern struct queue* tail;
+
 void test_push();
 
 int main() {
-    printf("TEST1 PUSH");
+    printf("TEST1 PUSH\n");
     test_push();
     return 0;
 }
@@ -14,12 +17,13 @@ void test_push() {
     queue* node = NULL;
 
     int value1 = 10, value2 = 20, value3 = 30;
-    push_back(&node, value1);
-    push_back(&node, value2);
-    push_back(&node, value3);
+    node = push_back(value1);
+    node = push_back(value2);
+    node = push_back(value3);
 
     if(tail != NULL) {
-        printf("Value %d\n", node->next->value);
+        printf("Value %d\n", node->value);
+        printf("Head value: %d\n", head->value);
         printf("SUCCESS");
     } else {
         printf("FAIL");
