c56d7a340320886e11d51692d42927a33ac75501
Michael <mvasilyev04@gmail.com>
2024-10-29 18:52:14 +0300

    change struct queue

--- src/queue.h
+++ src/queue.h
@@ -1,17 +1,19 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <stdio.h>
+
 /*
     We need to use 2 pointers, 1 to first element, 2 to last element, 
 */
 
-typedef struct List {
-    int value;
-    struct list* next;
-} List;
-
 typedef struct queue {
-    struct List* frnt, *rear;
+    int value;
+    struct queue* next;
 } queue;
+
+struct queue* head = NULL, *tail = NULL;
+
+
 
 #endif
